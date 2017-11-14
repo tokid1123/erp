e289d358468e2c085afa9a3ac120f4fe7f246125
tokid1123 <tokid1123@163.com>
2017-11-14 17:05:01 +0800

    test

---
+++ src/main/java/ManageApplication.java
@@ -0,0 +1,19 @@
+/*
+* @Description: TODO
+* @author king
+* @date 2017/11/14 17:03
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+@SpringBootApplication
+@Configuration
+@ServletComponentScan
+public class ManageApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(ManageApplication.class, args);
+    }
+}
