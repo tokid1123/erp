c0e635b1b54b3458b4a1c592c619175982ed0cd2
tokid1123 <tokid1123@163.com>
2017-11-30 17:25:01 +0800

    .

--- src/main/java/com/tokid/base/config/shiro/ShiroConfig.java
+++ src/main/java/com/tokid/base/config/shiro/ShiroConfig.java
@@ -1,6 +1,6 @@
 package com.tokid.base.config.shiro;
 /*
-* @Description: TODO
+* @Description:
 * @author king
 * @date 2017/11/15 10:21
 *
--- src/main/java/com/tokid/base/config/shiro/filter/JWTOrAuthenticationFilter.java
+++ src/main/java/com/tokid/base/config/shiro/filter/JWTOrAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.tokid.base.config.shiro.filter;
 /*
-* @Description: TODO
+* @Description:
 * @author king
 * @date 2017/11/16 16:00
 */
